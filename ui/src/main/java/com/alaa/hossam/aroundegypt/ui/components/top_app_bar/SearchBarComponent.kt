745492ae1e8da58fb9377d993317998c065a27c7
Alaa Hossam <alaa.m.hossam@gmail.com>
2025-07-08 14:34:43 +0300

    - Renamed `TopAppBar.kt` to `SearchBarComponent.kt` and create `TopAppBarComponent.kt`. - A new file `TopAppBarComponent.kt`

--- ui/src/main/java/com/alaa/hossam/aroundegypt/ui/components/top_app_bar/SearchBarComponent.kt
+++ ui/src/main/java/com/alaa/hossam/aroundegypt/ui/components/top_app_bar/SearchBarComponent.kt
@@ -5,12 +5,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun TopAppBar(modifier: Modifier = Modifier) {
-    
+fun SearchBarComponent(modifier: Modifier = Modifier) {
+
 }
 
 @Preview
 @Composable
-private fun TopAppBarPreview() {
-        TopAppBar()
+private fun SearchBarComponentPreview() {
+    SearchBarComponent()
 }
---
+++ ui/src/main/java/com/alaa/hossam/aroundegypt/ui/components/top_app_bar/TopAppBarComponent.kt
@@ -0,0 +1,33 @@
+package com.alaa.hossam.aroundegypt.ui.components.top_app_bar
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.alaa.hossam.aroundegypt.ui.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarComponent(modifier: Modifier = Modifier) {
+    Row {
+        IconButton(
+            onClick = {},
+            content = {
+                Icon(
+                    painter = painterResource(R.drawable.icn_menu),
+                    contentDescription = null
+                )
+            })
+
+    }
+}
+
+@Preview
+@Composable
+private fun TopAppBarComponentPreview() {
+    TopAppBarComponent()
+}
