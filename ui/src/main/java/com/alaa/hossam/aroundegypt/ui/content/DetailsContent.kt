88da68fa496aafc29b4bf15453e5be9ae70b903d
Alaa Hossam <alaa.m.hossam@gmail.com>
2025-07-08 19:07:59 +0300

    - Add share and favorite icons.
    The `DetailsContent` composable now includes:
    - `PlaceDetailsTopComponent`.
    - Place name and location.
    - Share and favorite `IconButton`s with a like count.
    - A `HorizontalDivider`.
    - A description section.
    - New drawable resources for `icn_action_favorite` and `icn_share` in various densities.

--- ui/src/main/java/com/alaa/hossam/aroundegypt/ui/content/DetailsContent.kt
+++ ui/src/main/java/com/alaa/hossam/aroundegypt/ui/content/DetailsContent.kt
@@ -1,17 +1,65 @@
 package com.alaa.hossam.aroundegypt.ui.content
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.alaa.hossam.aroundegypt.ui.R
+import com.alaa.hossam.aroundegypt.ui.components.place_details.PlaceDetailsTopComponent
 
 @Composable
 fun DetailsContent(modifier: Modifier = Modifier) {
 
     Column {
+        PlaceDetailsTopComponent()
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth().padding(14.dp)
+        ) {
+            Column {
+                Text("Abu Simbel Temples")
+                Text("Aswan, Egypt.")
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                IconButton(
+                    onClick = {},
+                    content = {
+                        Image(
+                            painter = painterResource(R.drawable.icn_share),
+                            contentDescription = null
+                        )
+                    })
 
+                IconButton(
+                    onClick = {},
+                    content = {
+                        Image(
+                            painter = painterResource(R.drawable.icn_action_favorite),
+                            contentDescription = null
+                        )
+                    })
+
+                Text("45")
+            }
+
+        }
+        HorizontalDivider(modifier = Modifier.padding(horizontal = 14.dp))
+        Column(modifier = Modifier.padding(14.dp)) {
+            Text("Description")
+            Text("The Abu Simbel temples are two massive rock temples at Abu Simbel, a village in Nubia, southern Egypt, near the border with Sudan. They are situated on the western bank of Lake Nasser, about 230 km southwest of Aswan (about 300 km by road). The twin temples were originally carved out of the mountainside in the 13th century BC, during the 19th dynasty reign of the Pharaoh Ramesses II. They serve as a lasting monument to the king and his queen Nefertari, and commemorate his victory at the Battle of Kadesh. Their huge external rock relief figures have become iconic.")
+        }
     }
-
 }
 
 @Preview
