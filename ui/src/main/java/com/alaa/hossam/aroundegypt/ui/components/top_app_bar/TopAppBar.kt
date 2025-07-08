625288f4a9b3dd6c66ba7eac3059748787e0af47
Alaa Hossam <alaa.m.hossam@gmail.com>
2025-07-08 14:24:06 +0300

    - Add `TopAppBar.kt` Composable function. - Add `icn_menu.webp` to drawable resources in different densities. - Add `/.idea/misc.xml` to `.gitignore`. - Remove XML declaration from `.idea/misc.xml`.

---
+++ ui/src/main/java/com/alaa/hossam/aroundegypt/ui/components/top_app_bar/TopAppBar.kt
@@ -0,0 +1,16 @@
+package com.alaa.hossam.aroundegypt.ui.components.top_app_bar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    
+}
+
+@Preview
+@Composable
+private fun TopAppBarPreview() {
+        TopAppBar()
+}
