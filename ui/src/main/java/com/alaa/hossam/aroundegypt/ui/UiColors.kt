8f5ec704af0fb532c8cd803676c5d784d637b039
Alaa Hossam <alaa.m.hossam@gmail.com>
2025-07-08 14:48:03 +0300

    - Implement `SearchBarComponent` with `TextField`. - Add `searchBgColor` to `UiColors.kt`. - Use `searchBgColor` in `SearchBarComponent` for focused and unfocused container colors. - Set placeholder text to "Try Luxor". - Add search icon as leading icon.

---
+++ ui/src/main/java/com/alaa/hossam/aroundegypt/ui/UiColors.kt
@@ -0,0 +1,5 @@
+package com.alaa.hossam.aroundegypt.ui
+
+import androidx.compose.ui.graphics.Color
+
+val searchBgColor = Color(0x1F8E8E93)
--- ui/src/main/java/com/alaa/hossam/aroundegypt/ui/components/top_app_bar/SearchBarComponent.kt
+++ ui/src/main/java/com/alaa/hossam/aroundegypt/ui/components/top_app_bar/SearchBarComponent.kt
@@ -1,12 +1,36 @@
 package com.alaa.hossam.aroundegypt.ui.components.top_app_bar
 
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.alaa.hossam.aroundegypt.ui.R
+import com.alaa.hossam.aroundegypt.ui.searchBgColor
 
 @Composable
 fun SearchBarComponent(modifier: Modifier = Modifier) {
-
+    TextField(
+        value = "",
+        onValueChange = {},
+        shape = MaterialTheme.shapes.large,
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = searchBgColor,
+            unfocusedContainerColor = searchBgColor
+        ),
+        placeholder = { Text(text = "Try Luxor") },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(R.drawable.icn_search),
+                contentDescription = null
+            )
+        }
+    )
 }
 
 @Preview
