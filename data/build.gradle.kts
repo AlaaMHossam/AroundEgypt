b8ce1827f0ff73ba6ea40f37c93af91092db89ef
Alaa Hossam <alaa.m.hossam@gmail.com>
2025-07-09 07:32:14 +0300

    - In the `data` module's `build.gradle.kts` file:     - Added `BASE_URL` build configuration field for `debug` and `release` build types, set to `"https://aroundegypt.34ml.com"`.     - Updated Java compatibility to version 21.     - Added `optIn.add("kotlin.RequiresOptIn")` to compiler options.     - Enabled `buildConfig`.     - Added `retrofit` and `kotlinx-serialization-json` dependencies. - In `gradle/libs.versions.toml`:     - Added versions for `retrofit` (3.0.0) and `kotlinxSerializationJson` (1.9.0).     - Added library entries for `retrofit` and `kotlinx-serialization-json`.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -15,20 +15,31 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "BASE_URL", "\"https://aroundegypt.34ml.com\"")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            buildConfigField("String", "BASE_URL", "\"https://aroundegypt.34ml.com\"")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        target {
+            compilerOptions {
+                optIn.add("kotlin.RequiresOptIn")
+            }
+        }
+    }
+    buildFeatures {
+       buildConfig = true
     }
 }
 
@@ -39,6 +50,8 @@ dependencies {
     implementation(libs.material)
     implementation(project(":domain"))
     implementation(project(":common_utils"))
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
